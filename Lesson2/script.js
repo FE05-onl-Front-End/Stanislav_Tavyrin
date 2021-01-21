ed830578653c5a60914785749ac94332f0966e22
Stanislau <omgnoooob@gmail.com>
2021-01-21 22:06:58 +0300

    feat: add script realization

---
+++ Lesson2/script.js
@@ -0,0 +1,10 @@
+let data = {
+    name:  "Артур",
+    ourLost: 1000,
+    ourArmy: 5000,
+    enemiesLost: 10000,
+}
+
+console.log(`Король ${data.name}, мы победили! Враг бежит, оставив на поле боя ${data.enemiesLost} павших воинов от всей его армии. Наши потери король ${data.name} составили ${data.ourLost} героически павших воинов от всей нашей армии в ${data.ourArmy} солдат! Мы победили, сохранив ${data.ourArmy - data.ourLost} воинов. С победой король ${data.name}!!`)
+
+
