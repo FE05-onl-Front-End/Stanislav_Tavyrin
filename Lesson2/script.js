2325807254b24a7101cfcc91dbd44edcca2ecaee
Stanislau <omgnoooob@gmail.com>
2021-01-22 18:50:22 +0300

    refactor: change input for object data

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -1,10 +1,11 @@
-let data = {
-    name:  "Артур",
-    ourLost: 1000,
-    ourArmy: 5000,
-    enemiesLost: 10000,
-}
 
-console.log(`Король ${data.name}, мы победили! Враг бежит, оставив на поле боя ${data.enemiesLost} павших воинов от всей его армии. Наши потери король ${data.name} составили ${data.ourLost} героически павших воинов от всей нашей армии в ${data.ourArmy} солдат! Мы победили, сохранив ${data.ourArmy - data.ourLost} воинов. С победой король ${data.name}!!`)
+let data = {};
+data.name = prompt('Имя короля?');
+data.ourLost = +prompt('Потери короля?');
+data.ourArmy = +prompt('сколько было воинов в армии короля?');
+data.enemiesArmy = +prompt('Сколько было врагов?');
+data.enemiesLost = +prompt('Сколько враги потеряли войнов?');
+
+console.log(`Король ${data.name}, мы победили вражескую армию в ${data.enemiesArmy}! Враг бежит, оставив на поле боя ${data.enemiesLost} павших воинов от всей его армии. Наши потери король ${data.name} составили ${data.ourLost} героически павших воинов от всей нашей армии в ${data.ourArmy} солдат! Мы победили, сохранив ${data.ourArmy - data.ourLost} воинов и можем продолжить преследовать остатки вражеской армии в ${data.enemiesArmy - data.enemiesLost} солдат. С победой король ${data.name}!!`)
 
 
