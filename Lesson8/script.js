8f59329b44d15900fd1892133c96fc09c386b72f
Stanislau <omgnoooob@gmail.com>
2021-02-13 00:20:53 +0300

    feat: complete tasks whith modal form

--- Lesson8/script.js
+++ Lesson8/script.js
@@ -1,21 +1,3 @@
-// const random = () => {
-//     return Math.floor((Math.random() * 100) + 1);
-// }
-
-// function randomArr() {
-
-//     let newArr = [];
-//     return function () {
-//         while (newArr.length < 100) {
-//             let randomNum = random();
-//             newArr.includes(randomNum) ? true : newArr.push(randomNum);
-//         }
-//         return newArr;
-//     }
-// }
-
-// let array = randomArr();
-// console.log(array());
 
 let modal = document.querySelector('.wrapper');
 let firstName = document.querySelector('input[name=first-name]');
@@ -23,43 +5,70 @@ let lastName = document.querySelector('input[name=last-name]');
 let age = document.querySelector('input[name=age]');
 let form = document.querySelector('#userform');
 let incorrect = document.querySelectorAll('.incorrect');
-function show() {
+let okBtn = document.querySelector('.ok');
 
+function show() {
     modal.style.display = 'block';
 }
 
 function hide() {
     modal.style.display = 'none';
-
+    firstName.value = '';
+    lastName.value = '';
+    age.value = '';
+    incorrect.forEach(elem => elem.style.display = 'none');
 }
+
 function checkNameInput() {
 
     if (firstName.value.match(/\d/g) || firstName.value.length > 10) {
         incorrect[0].style.display = 'block';
-        document.querySelector('.ok').disabled = true;
+        okBtn.disabled = true;
+    } else if (incorrect[1].style.display == 'block' || incorrect[2].style.display == 'block') {
+        incorrect[0].style.display = 'none';
+        okBtn.disabled = true;
     } else {
         incorrect[0].style.display = 'none';
-        document.querySelector('.ok').disabled = false;
+        okBtn.disabled = false;
     }
 
 }
+
 function checkLastNameInput() {
 
     if (lastName.value.match(/\d/g) || lastName.value.length > 10) {
         incorrect[1].style.display = 'block';
-        document.querySelector('.ok').disabled = true;
+        okBtn.disabled = true;
+
+    } else if (incorrect[0].style.display == 'block' || incorrect[2].style.display == 'block') {
+        incorrect[1].style.display = 'none';
+        okBtn.disabled = true;
     } else {
         incorrect[1].style.display = 'none';
-        document.querySelector('.ok').disabled = false;
+        okBtn.disabled = false;
     }
 
 }
 
+function checkAge() {
+    console.log(+age.value);
+    if (isNaN(+age.value) || +age.value < 0 || +age.value > 135) {
+        incorrect[2].style.display = 'block';
+        okBtn.disabled = true;
+
+    } else if (incorrect[0].style.display == 'block' || incorrect[1].style.display == 'block') {
+        incorrect[2].style.display = 'none';
+        okBtn.disabled = true;
+    } else {
+        incorrect[2].style.display = 'none';
+        okBtn.disabled = false;
+    }
+}
+
 form.addEventListener('submit', () => {
-    // event.preventDefault();
     alert(`
            Имя: ${firstName.value}
            Фамилия: ${lastName.value}
            Возраст: ${age.value}`);
-    // hide();
+
 })
